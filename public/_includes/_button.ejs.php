37f1cb77923191e7eb9f69519f83a5e4b0261f9a
nicksglobal <nicksglobal@gmail.com>
2018-01-31 15:10:06 -0500

    adding button file

---
+++ public/_includes/_button.ejs.php
@@ -0,0 +1,5 @@
+ 	<div class="row">
+      <div class="col-lg-12 col-xs-12" align="center" id="space">
+          <a href="https://www.cvent.com/events/2018-impact-seminar/registration-a7a6d8d1f5fd477cbf0ef66154254603.aspx?fqp=true" target="blank"><button class="register_color_2">Click Here to Register</button></a>
+      </div>
+    </div><!-- row -->
